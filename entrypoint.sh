c2eefb39d521752423a08a002205486fcfb0f7dc
Robin Knipe <robin.knipe@gmail.com>
2020-02-05 10:53:43 +0000

    Save results files to the output folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,9 +25,9 @@ NUM_THREADS=${NUM_THREADS:-1}
 THREAD_DELAY=${THREAD_DELAY:-5}
 if [ ${NUM_THREADS} -gt 1 ] ; then
   for i in $(seq 1 1 ${NUM_THREADS} ) ; do
-    artillery run -qo "./result-${i}.log" "${SCRIPT_DIR}/high-load.yml" &
+    artillery run -qo "./output/result-${i}.json" "${SCRIPT_DIR}/high-load.yml" &
     sleep ${THREAD_DELAY}
   done
 else
-  artillery run -o "./result.json" "${SCRIPT_DIR}/high-load.yml"
+  artillery run -o "./output/result.json" "${SCRIPT_DIR}/high-load.yml"
 fi
