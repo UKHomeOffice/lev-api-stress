f406f35b7ffd9b8076d95dafbe313330e14f491b
Robin Knipe <robin.knipe@gmail.com>
2020-02-05 08:50:04 +0000

    Change test output to JSON files

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,10 +24,10 @@ export TOKEN=`curl -fsS \
 NUM_THREADS=${NUM_THREADS:-1}
 THREAD_DELAY=${THREAD_DELAY:-5}
 if [ ${NUM_THREADS} -gt 1 ] ; then
-  for i in {1..${NUM_THREADS}} ; do
-    artillery run "${SCRIPT_DIR}/high-load.yml" > "./result-${i}.log" 2>&1 &
+  for i in $(seq 1 1 ${NUM_THREADS} ) ; do
+    artillery run -qo "./result-${i}.log" "${SCRIPT_DIR}/high-load.yml" &
     sleep ${THREAD_DELAY}
   done
 else
-  artillery run "${SCRIPT_DIR}/high-load.yml" > "./result.log" 2>&1 &
+  artillery run -o "./result.json" "${SCRIPT_DIR}/high-load.yml"
 fi
