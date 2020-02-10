f4dd86085ed42f0489334847ff810fb47f8bd43a
Robin Knipe <robin.knipe@gmail.com>
2020-02-10 21:59:04 +0000

    Setup basic test environment

---
+++ test/common.js
@@ -0,0 +1,9 @@
+'use strict';
+
+global.chai = require('chai')
+	.use(require('sinon-chai'))
+	.use(require('chai-fs'))
+	.use(require('chai-json'))
+	.use(require('chai-integer'));
+global.expect = chai.expect;
+global.sinon = require('sinon');
